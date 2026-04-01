f9452a051a4cdd364b1e4fafad5da8316fe0d951
Alice Oliveira <aliceoliver1905@gmail.com>
2026-04-01 10:36:03 -0300

    mudando nome de pastas
    só adicionei a pasta de dashboard e alterei o nome de "aluno" para "autenticação"

---
+++ mentoria-frontend/src/pages/dashboards/dashboardAluno.jsx
@@ -0,0 +1,14 @@
+function DashboardAluno() {
+  return (
+    <div>
+      <h1>Dashboard do Aluno</h1>
+      <p>Bem-vindo ao seu dashboard, aqui você pode acompanhar seu progresso, acessar materiais de estudo e interagir com seus mentores.</p>
+
+      <div>
+        
+      </div>
+    </div>
+   
+  )
+
+}
